94c95c75c5c8a738d17aebc28d0c911c1e558951
Pavun786 <pulsarpavuns786@gmail.com>
2024-09-25 15:26:11 +0530

    cors_configure

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-    origin : "http://localhost:3000"
+    origin : "https://ecommerce-landing-page-frontend.vercel.app"
 }))
 
 const port = 4600;
